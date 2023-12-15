10db739caa9903f303a291c640eeec36e82b53c7
David Rodrigues <150310025+david-oliv3ira@users.noreply.github.com>
2023-12-14 23:59:11 -0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-asdasd
+let nome = "David";
+let nivel = 999;
+
+if (nivel <= 1000) {
+    console.log(`O Herói de ${nome} está no nível de Ferro`);
+} else if (nivel >= 1001 && nivel <= 2000) {
+    console.log(`O Herói de ${nome} está no nível de Bronze`);
+} else if (nivel >= 2001 && nivel <= 6000) {
+    console.log(`O Herói de ${nome} está no nível de Prata`);
+} else if (nivel >= 6001 && nivel <= 7000) {
+    console.log(`O Herói de ${nome} está no nível de Ouro`);
+} else if (nivel >= 7001 && nivel <= 8000) {
+    console.log(`O Herói de ${nome} está no nível de Platina`);
+} else if (nivel >= 8001 && nivel <= 9000) {
+    console.log(`O Herói de ${nome} está no nível de Ascendente`);
+} else if (nivel >= 9001 && nivel <= 10000) {
+    console.log(`O Herói de ${nome} está no nível de Ascendente`);
+} else {
+    console.log(`O Herói de ${nome} está no nível de Radiante`);
+}
+
+
